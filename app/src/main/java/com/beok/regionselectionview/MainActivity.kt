04f425db85c1b01217fe93e34bf47eaf855d6dbe
강현석 <kekemusa37@gmail.com>
2021-04-02 23:05:45 +0900

    [구현] MainActivity

--- app/src/main/java/com/beok/regionselectionview/MainActivity.kt
+++ app/src/main/java/com/beok/regionselectionview/MainActivity.kt
@@ -2,10 +2,40 @@ package com.beok.regionselectionview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.beok.regionselectionview.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setupBinding()
+        setupRecyclerView()
+        showContent()
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvMainContent.adapter = BaseAdapter(
+            layoutResourceID = R.layout.rv_item_city,
+            bindingID = BR.area,
+            viewModel = mapOf(BR.viewModel to viewModel)
+        )
+    }
+
+    private fun showContent() {
+        viewModel.fetch()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
