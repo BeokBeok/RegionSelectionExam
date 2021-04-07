1d8ab01aa11250a56ad79310baf70a66f405d28c
강현석 <kekemusa37@gmail.com>
2021-04-07 23:05:19 +0900

    [적용] 단일 선택

--- app/src/main/java/com/beok/regionselectionview/MainViewModel.kt
+++ app/src/main/java/com/beok/regionselectionview/MainViewModel.kt
@@ -15,62 +15,68 @@ class MainViewModel @Inject constructor() : ViewModel() {
     private val _district = MutableStateFlow<List<Area>>(emptyList())
     val district: StateFlow<List<Area>> get() = _district
 
+    private val wholeCity = mutableListOf(
+        Area("서울특별시"),
+        Area("부산광역시"),
+    )
+
+    private val seoul = mutableListOf(
+        Area("전체"),
+        Area("강남구"),
+        Area("강동구"),
+        Area("강북구"),
+        Area("강서구"),
+        Area("관악구"),
+        Area("광진구"),
+        Area("구로구"),
+        Area("금천구"),
+        Area("노원구"),
+        Area("도봉구"),
+        Area("동대문구"),
+        Area("동작구"),
+        Area("마포구"),
+        Area("서대문구"),
+        Area("서초구"),
+        Area("성동구"),
+        Area("성북구"),
+        Area("송파구"),
+        Area("양천구"),
+        Area("영등포구"),
+        Area("용산구"),
+        Area("은평구"),
+        Area("종로구"),
+        Area("중구"),
+        Area("중랑구"),
+    )
+
+    private val busan = mutableListOf(
+        Area("전체"),
+        Area("중구"),
+        Area("서구"),
+        Area("동구"),
+        Area("영도구"),
+        Area("부산진구"),
+        Area("동래구"),
+        Area("남구"),
+        Area("북구"),
+        Area("해운대구"),
+        Area("사하구"),
+        Area("금정구"),
+        Area("강서구"),
+        Area("연제구"),
+        Area("수영구"),
+        Area("사상구"),
+        Area("기장군"),
+    )
+
     fun fetchCity() {
-        _city.value = listOf(
-            Area("서울특별시"),
-            Area("부산광역시"),
-        )
+        _city.value = wholeCity
     }
 
     fun fetchDistrict(city: Area) {
         _district.value = when (city.name) {
-            "서울특별시" -> listOf(
-                Area("전체"),
-                Area("강남구"),
-                Area("강동구"),
-                Area("강북구"),
-                Area("강서구"),
-                Area("관악구"),
-                Area("광진구"),
-                Area("구로구"),
-                Area("금천구"),
-                Area("노원구"),
-                Area("도봉구"),
-                Area("동대문구"),
-                Area("동작구"),
-                Area("마포구"),
-                Area("서대문구"),
-                Area("서초구"),
-                Area("성동구"),
-                Area("성북구"),
-                Area("송파구"),
-                Area("양천구"),
-                Area("영등포구"),
-                Area("용산구"),
-                Area("은평구"),
-                Area("종로구"),
-                Area("중구"),
-                Area("중랑구"),
-            )
-            "부산광역시" -> listOf(
-                Area("전체"),
-                Area("중구"),
-                Area("서구"),
-                Area("동구"),
-                Area("영도구"),
-                Area("부산진구"),
-                Area("동래구"),
-                Area("남구"),
-                Area("북구"),
-                Area("해운대구"),
-                Area("사하구"),
-                Area("금정구"),
-                Area("강서구"),
-                Area("연제구"),
-                Area("수영구"),
-                Area("사상구"),
-                Area("기장군"),
-            )
+            "서울특별시" -> seoul
+            "부산광역시" -> busan
             else -> emptyList()
         }
     }
@@ -84,7 +90,7 @@ class MainViewModel @Inject constructor() : ViewModel() {
                             fetchDistrict(area)
                             return@map area.copy(isSelected = !it.isSelected)
                         }
-                        it
+                        it.copy(isSelected = false)
                     }
             }
             AreaScope.DISTRICT -> {
@@ -93,7 +99,7 @@ class MainViewModel @Inject constructor() : ViewModel() {
                         if (it.name == area.name) {
                             return@map area.copy(isSelected = !it.isSelected)
                         }
-                        it
+                        it.copy(isSelected = false)
                     }
             }
         }
